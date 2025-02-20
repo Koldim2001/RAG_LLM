afd4255a958b36682cf8e9cd34302bf87eb64041
Dmitry Kolesnikov <koldim2001@yandex.ru>
2025-02-20 11:10:17 +0300

    new func with chat

--- chat.py
+++ chat.py
@@ -1,8 +1,13 @@
 import streamlit as st
 from services.AskLLM import AskLLM
 
-# Инициализация AskLLM
-ask = AskLLM()
+# Инициализация AskLLM через @st.cache_resource
+@st.cache_resource
+def initialize_ask_llm():
+    return AskLLM()
+
+# Получаем объект AskLLM
+ask = initialize_ask_llm()
 
 # Инициализация состояния сессии для хранения истории чата Streamlit
 if "chat_history" not in st.session_state:
@@ -10,9 +15,28 @@ if "chat_history" not in st.session_state:
 
 # Левая панель (sidebar) для выбора collection_db_name
 with st.sidebar:
-    collection_db_name = st.text_input("RAG коллекция", value="", placeholder="Введите имя коллекции в БД")
-    if not collection_db_name:
-        collection_db_name = None  # Если поле пустое, устанавливаем значение None
+    # Проверяем, есть ли список коллекций в st.session_state
+    if "collections" not in st.session_state:
+        # Если нет, получаем список коллекций из Milvus и сохраняем его в st.session_state
+        st.session_state.collections = ask.vector_db_node.list_milvus_collections()
+    
+    # Добавляем опцию "Без коллекции" в начало списка
+    collections = ["Без коллекции"] + st.session_state.collections
+    
+    # Создание выпадающего меню
+    selected_collection = st.selectbox(
+        "Выберите RAG коллекцию",
+        collections,
+        index=0  # По умолчанию выбирается "Без коллекции"
+    )
+    
+    # Установка collection_db_name
+    if selected_collection == "Без коллекции":
+        collection_db_name = None
+        st.markdown("__Mодель будет отвечать без RAG__")
+    else:
+        collection_db_name = selected_collection
+
 
 # Отображение истории чата
 for message in st.session_state.chat_history:
@@ -31,7 +55,7 @@ if prompt := st.chat_input("Задайте ваш вопрос:"):
         st.session_state.previous_messages = []  # Инициализация внутренней истории для AskLLM
 
     # Обработка запроса через AskLLM
-    message_number = (len(st.session_state.chat_history) - 1) // 2 # Номер текущего сообщения
+    message_number = (len(st.session_state.chat_history) - 1) // 2  # Номер текущего сообщения
     res = ask.process(prompt, message_number, collection_db_name, st.session_state.previous_messages)
 
     # Получение ответа модели
